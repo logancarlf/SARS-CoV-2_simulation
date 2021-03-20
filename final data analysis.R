7f84a7fa32ca67eb52bcf440e0533d8f552a5b95
Elizabeth233333333 <mingjun.sun19@imperial.ac.uk>
2021-03-20 19:17:40 +0000

    Update final data analysis.R

--- final data analysis.R
+++ final data analysis.R
@@ -60,6 +60,18 @@ ggplot(data, aes(Date, diff_deaths_prop)) +
   ggtitle("Difference between cumulative deaths of our model against goverment data") + 
   theme(plot.title = element_text(hjust = 0.5)) 
 
+#outliers of the proportional difference between cumulative number of deaths of our 
+#model and goverment data
+iqr_diff_deaths <- IQR(data$diff_deaths)
+lowerq_diff_deaths = quantile(data$diff_deaths)[2]
+upperq_diff_deaths = quantile(data$diff_deaths)[4]
+outlierlower_diff_deaths = lowerq_diff_deaths - (iqr_diff_deaths * 1.5)
+outlierupper_diff_deaths = upperq_diff_deaths + (iqr_diff_deaths * 1.5)
+lower_outliers_diff_deaths <- which(data$diff_deaths < outlierlower_diff_deaths)
+upper_outliers_diff_deaths <- which(data$diff_deaths > outlierupper_diff_deaths)
+print(lower_outliers_diff_deaths)
+print(upper_outliers_diff_deaths)
+
 # calculating proportional difference between cumulative number of infections of our 
 # model and goverment data
 data$diff_cases <- abs(data$mod_inf - data$cumInfected)
@@ -73,6 +85,20 @@ ggplot(data, aes(Date, diff_cases_prop)) +
   ggtitle("Difference between cumulative infected individuals of our model against goverment data") + 
   theme(plot.title = element_text(hjust = 0.5)) 
 
+#outliers of the proportional difference between cumulative number of infections of our 
+#model and goverment data
+iqr_diff_cases <- IQR(data$diff_cases)
+lowerq_diff_cases = quantile(data$diff_cases)[2]
+upperq_diff_cases = quantile(data$diff_cases)[4]
+outlierlower_diff_cases = lowerq_diff_cases - (iqr_diff_cases * 1.5)
+outlierupper_diff_cases = upperq_diff_cases + (iqr_diff_cases * 1.5)
+lower_outliers_diff_cases <- which(data$diff_cases < outlierlower_diff_cases)
+upper_outliers_diff_cases <- which(data$diff_cases > outlierupper_diff_cases)
+print(lower_outliers_diff_cases)
+print(upper_outliers_diff_cases)
+
+# calculating proportional difference between cumulative number of vaccinations of our 
+# model and goverment data
 data$diff_vacc <- abs(data$mod_vacc - data$cumPplVac)
 data$diff_vacc_prop <- data$diff_vacc/data$cumPplVac
 ggplot(data, aes(Date, diff_vacc_prop)) + 
@@ -83,3 +109,14 @@ ggplot(data, aes(Date, diff_vacc_prop)) +
   ggtitle("Difference between cumulative number of vaccinations of our model against goverment data") + 
   theme(plot.title = element_text(hjust = 0.5)) 
 
+#outliers of the proportional difference between cumulative number of vaccinations of our 
+#model and goverment data
+iqr_diff_vacc <- IQR(data$diff_vacc)
+lowerq_diff_vacc = quantile(data$diff_vacc)[2]
+upperq_diff_vacc = quantile(data$diff_vacc)[4]
+outlierlower_diff_vacc = lowerq_diff_vacc - (iqr_diff_vacc * 1.5)
+outlierupper_diff_vacc = upperq_diff_vacc + (iqr_diff_vacc * 1.5)
+lower_outliers_diff_vacc <- which(data$diff_vacc < outlierlower_diff_vacc)
+upper_outliers_diff_vacc <- which(data$diff_vacc > outlierupper_diff_vacc)
+print(lower_outliers_diff_vacc)
+print(upper_outliers_diff_vacc)
